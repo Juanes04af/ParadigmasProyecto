c9dbdf2707e40b2218f4c4c422f44d6fdcec553a
Laura Sofia Hernandez <ing.laurasofiax04@gmail.com>
2025-04-08 07:45:13 -0500

    vehiculos

--- co/edu/poli/paradigmas/tc/proyecto/entities/Vehiculo.java
+++ co/edu/poli/paradigmas/tc/proyecto/entities/Vehiculo.java
@@ -1,30 +1,61 @@
 package co.edu.poli.paradigmas.tc.proyecto.entities;
 
 public class Vehiculo {
-
-    private int NumeroPlaca;
-    private String Modelo;
-    private byte NumeroPasajeros;
+    private int numeroPlaca;
+    private String modelo;
+    private byte numeroPasajeros;
     private Rutas ruta;
-    private boolean Disponibilidad;
-    private boolean DisponibilidadConductor;
-    public Vehiculo(int numeroPlaca, String modelo, byte numeroPasajeros, Rutas ruta, boolean disponibilidad,
-                     boolean disponibilidadConductor) {
-        super();
-        NumeroPlaca = numeroPlaca;
-        Modelo = modelo;
-        NumeroPasajeros = numeroPasajeros;
+    private boolean disponibilidad;
+    private boolean disponibilidadConductor;
+
+    public Vehiculo(int numeroPlaca, String modelo, byte numeroPasajeros, Rutas ruta,
+                    boolean disponibilidad, boolean disponibilidadConductor) {
+        this.numeroPlaca = numeroPlaca;
+        this.modelo = modelo;
+        this.numeroPasajeros = numeroPasajeros;
         this.ruta = ruta;
-        Disponibilidad = disponibilidad;
-        DisponibilidadConductor = disponibilidadConductor;
+        this.disponibilidad = disponibilidad;
+        this.disponibilidadConductor = disponibilidadConductor;
     }
 
-    public boolean isDisponibilidad() {
-        return Disponibilidad;
+    public boolean isDisponibilidadConductor() {
+        return disponibilidadConductor;
     }
 
-    public void setDisponibilidad(boolean disponibilidad) {
-        Disponibilidad = disponibilidad;
+    public void setDisponibilidadConductor(boolean disponibilidadConductor) {
+        this.disponibilidadConductor = disponibilidadConductor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public byte getNumeroPasajeros() {
+        return numeroPasajeros;
+    }
+
+    public void setNumeroPasajeros(byte numeroPasajeros) {
+        this.numeroPasajeros = numeroPasajeros;
+    }
+
+    public int getNumeroPlaca() {
+        return numeroPlaca;
+    }
+
+    public void setNumeroPlaca(int numeroPlaca) {
+        this.numeroPlaca = numeroPlaca;
+    }
+
+    public Rutas getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(Rutas ruta) {
+        this.ruta = ruta;
     }
 
 }
