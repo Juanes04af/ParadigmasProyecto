c74b20f1843babcc2c5a628709d8b141d55eda41
Laura Sofia Hernandez <ing.laurasofiax04@gmail.com>
2025-04-09 22:25:03 -0500

    gestionar vehiculos

---
+++ co/edu/poli/paradigmas/tc/proyecto/negocio/GestionarVehiculos.java
@@ -0,0 +1,58 @@
+package co.edu.poli.paradigmas.tc.proyecto.negocio;
+
+import co.edu.poli.paradigmas.tc.proyecto.entities.Vehiculo;
+import java.util.ArrayList;
+
+public class GestionarVehiculos {
+    private ArrayList<Vehiculo> vehiculos;
+
+    public GestionarVehiculos(ArrayList<Vehiculo> vehiculos) {
+        vehiculos = new ArrayList<>();
+    }
+
+    //Metodos CRUD
+    // Crear
+    public void agregarVehiculo (Vehiculo vehiculo) {
+        vehiculos.add(vehiculo);
+    }
+
+    // Leer
+    public void mostrarVehiculos() {
+        if (vehiculos.isEmpty()) {
+            System.out.println("No hay vehículos registrados.");
+        } else {
+            for (Vehiculo vehiculo : vehiculos) {
+                System.out.println(vehiculo);
+            }
+        }
+    }
+
+    // Actualizar
+    public boolean actualizarVehiculo(int numeroPlaca, boolean estaEnTaller) {
+        Vehiculo vehiculo = buscarVehiculo(numeroPlaca);
+        if (vehiculo != null) {
+            vehiculo.setDisponibilidad(!estaEnTaller);
+            return true;
+        }
+        return false;
+    }
+
+    public Vehiculo buscarVehiculo(int numeroPlaca) {
+        for (Vehiculo vehiculoTemp : vehiculos) {
+            if (vehiculoTemp.getNumeroPlaca() == numeroPlaca) {
+                return vehiculoTemp;
+            }
+        }
+        return null;
+    }
+
+    // Eliminar
+    public boolean eliminarVehiculo(int numeroPlaca) {
+        Vehiculo vehiculo = buscarVehiculo(numeroPlaca);
+        if (vehiculo != null) {
+            vehiculos.remove(vehiculo);
+            return true;
+        }
+        return false;
+    }
+}
