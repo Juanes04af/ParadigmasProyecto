179b9d1476423589d15c40f00012673554faae23
Laura Sofia Hernandez <ing.laurasofiax04@gmail.com>
2025-04-18 18:55:31 -0500

    empleado

--- co/edu/poli/paradigmas/tc/proyecto/entities/Empleados.java
+++ co/edu/poli/paradigmas/tc/proyecto/entities/Empleados.java
@@ -1,10 +1,8 @@
 package co.edu.poli.paradigmas.tc.proyecto.entities;
 
 public class Empleados extends Persona{
-
     public Empleados(int id, String nombre) {
         super(id, nombre);
     }
-
-
+    
 }
