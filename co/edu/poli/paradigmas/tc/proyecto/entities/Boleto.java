a021386e92a4d8dafe2404bff5cda8ccf7964102
Laura Sofia Hernandez <ing.laurasofiax04@gmail.com>
2025-04-18 20:40:17 -0500

    ruta

--- co/edu/poli/paradigmas/tc/proyecto/entities/Boleto.java
+++ co/edu/poli/paradigmas/tc/proyecto/entities/Boleto.java
@@ -5,12 +5,45 @@ public class Boleto {
     private long NumeroID;
     private String Nombre;
     private boolean CompraBoletos;
+    private String ruta;
 
-    public Boleto(long numeroID, String nombre, boolean compraBoletos) {
-        super();
-        NumeroID = numeroID;
+
+    public Boleto(boolean compraBoletos, String nombre, long numeroID, String ruta) {
+        CompraBoletos = compraBoletos;
         Nombre = nombre;
+        NumeroID = numeroID;
+        this.ruta = ruta;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public boolean isCompraBoletos() {
+        return CompraBoletos;
+    }
+
+    public void setCompraBoletos(boolean compraBoletos) {
         CompraBoletos = compraBoletos;
     }
 
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public long getNumeroID() {
+        return NumeroID;
+    }
+
+    public void setNumeroID(long numeroID) {
+        NumeroID = numeroID;
+    }
 }
